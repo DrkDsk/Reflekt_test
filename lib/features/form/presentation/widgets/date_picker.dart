d5fe6447f764b99205357691e6cc74fb11e7643b
Alfredo Palacios <joseaph.1998@gmail.com>
2024-06-08 09:03:55 -0600

    date picker added to form_body

--- lib/features/form/presentation/widgets/date_picker.dart
+++ lib/features/form/presentation/widgets/date_picker.dart
@@ -1,13 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:reflekt_test/core/constants/color.dart';
 
-class DatePicker extends StatelessWidget {
-  const DatePicker({super.key});
+class DatePicker extends StatefulWidget {
+
+  const DatePicker({
+    super.key
+  });
+
+  @override
+  State<DatePicker> createState() => _DatePickerState();
+}
+
+class _DatePickerState extends State<DatePicker> {
+
+  DateTime? selectedDate;
+  String dateFormatted = "";
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
-      height: 50,
+
+    Future<void> selectDate(BuildContext context) async {
+      final DateTime? picked = await showDatePicker(
+          context: context,
+          initialDate: selectedDate,
+          firstDate: DateTime(2015, 8),
+          lastDate: DateTime(2101));
+      if (picked != null && picked != selectedDate) {
+        setState(() {
+          selectedDate = picked;
+          String? day = selectedDate?.day.toString().padLeft(2, '0');
+          String? month = selectedDate?.month.toString().padLeft(2, '0');
+          String? year = selectedDate?.year.toString();
+          dateFormatted = '$day/$month/$year';
+        });
+      }
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Date of Birth",
+            style: TextStyle(
+                color: textColor
+            ),
+          ),
+          const SizedBox(height: 12),
+          GestureDetector(
+            onTap: () {
+              selectDate(context);
+            },
+            child: Container(
+              height: 60,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: textColor,
+                  ),
+                  borderRadius: BorderRadius.circular(8)
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                        dateFormatted.isNotEmpty ? dateFormatted : "mm/dd/yyyy",
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: textColor.withOpacity(0.5),
+                            fontWeight: FontWeight.w400
+                        )
+                    ),
+                    const Icon(
+                      Icons.calendar_month,
+                      color: textColor,
+                      size: 30,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
